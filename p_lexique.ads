7dbd1d750f7d7a4ed8c0fa2da44a010364cfb60f
DAI Haozhou <daiha@hostname.iut2.upmf-grenoble.fr>
2018-06-18 09:32:10 +0200

    ads3

--- p_lexique.ads
+++ p_lexique.ads
@@ -11,6 +11,9 @@ type TV_lexique is array(Integer range<>) of TR_mot;
 function Nb_Mots(nomfic : in String)  return Integer;
 --{}=>{résultat = nombre de lignes du fichier Nomfic et donc de mots dans le lexique correspondant}
 
+procedure Init_Lexique(Nomfic: in String; L : out Tv_lexique);
+--{On suppose que la taille du vecteur L correspond exactement au nombre de mots contenus dans le fichier} => {Range dans le vecteur  L, les mots contenus dans le fichier lexique Nomfic et les poids associés}
+
 function Poids_Mot(M  : in String; L : in Tv_lexique) return integer ;
 --{} => {résultat = le poids de M dans le lexique L est 0 si le mot n'est pas présent dans le lexique}
 
